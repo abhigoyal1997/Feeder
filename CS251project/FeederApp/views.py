9f184c060a46477116d8b361c5898e2750432518
Abhinav Goyal <abhigoyal@cse.iitb.ac.in>
2016-11-02 21:01:28 +0530

    change directory structure

--- CS251project/FeederApp/views.py
+++ CS251project/FeederApp/views.py
@@ -24,9 +24,11 @@ def login(request):
 			return HttpResponse("Student is logged in")
 			# We can also logout the person
 		return HttpResponse("How??")
-	return render(request,'login.html',{})    
+	return render(request,'login.html',{})
+
 def signup(request):
 	return render(request,'signup.html',{})
+	
 def register(request):
 	firstname = request.POST['firstname']
 	lastname = request.POST['lastname']
@@ -101,7 +103,7 @@ def admin_home(request):
 			return HttpResponse('Page not Found')
 		return render(request,'admin_home.html',{
 			'admin':request.user
-		})
+			})
 	except Exception as e:
 		return HttpResponse('Does not exist')
 
@@ -112,7 +114,9 @@ def ins_home(request):
 		user = User.objects.get(username=request.user.username)
 		if not request.user.username[0] == "i":
 			return HttpResponse('Page not Found')
-		return render(request,'instructor_home.html',{})
+		return render(request,'instructor_home.html',{
+			'instructor':request.user
+			})
 	except Exception as e:
 		return HttpResponse('Page not Found')
 
@@ -161,7 +165,7 @@ def make_course(request):
 	if request.user.username[0] == "a":
 		cname = request.POST['cname']
 		ccode = request.POST['ccode']
-		duration = request.POST.get('duration')
+		duration = request.POST.get('semester')
 		duration = int(duration)/2.0
 		branch = request.POST.get('branch')
 		credits = request.POST.get('credit')
@@ -233,6 +237,21 @@ def update_admin(request):
 	else:
 		return HttpResponse("Don't try to be smart!! We ensure quite enough security!! :)")
 
+def update_ins(request):
+	if request.user.username[0] == "i":
+		firstname = request.POST['firstname']
+		lastname = request.POST['lastname']
+		email = request.POST['email']
+		user.first_name = firstname
+		user.last_name = lastname
+		user.email = email
+		user.username = 'i:'+email
+		user.save()
+		messages.success(request, 'Profile Successfully Updated')
+		return redirect('ins_home')
+	else:
+		return HttpResponse("Don't try to be smart!! We ensure quite enough security!! :)")
+
 def add_feedback(request):
 	if request.user.username[0] == "i":
 		return render(request,'add_feedback.html',{
@@ -267,7 +286,7 @@ def view_deadline(request):
 
 def instprofile(request):
 	if request.user.username[0] == "i":
-		return render(request,'inst_profile.html',{})
+		return render(request,'ins_profile.html',{})
 	else:
 		return HttpResponse("Don't try to be smart!! We ensure quite enough security!! :)")
 
