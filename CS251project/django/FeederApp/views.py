160b36e4f3e62de0c2f6b58c13dab7d12e2518bb
Abhinav Goyal <abhigoyal@cse.iitb.ac.in>
2016-11-03 21:22:22 +0530

    a few more checks ;p

--- CS251project/django/FeederApp/views.py
+++ CS251project/django/FeederApp/views.py
@@ -180,115 +180,124 @@ def admin_profile(request):
 
 def make_course(request):
 	if request.user.is_authenticated and request.user.username[0] == "a":
-		cname = request.POST['cname']
-		ccode = request.POST['ccode']
-		if re.match('^[A-Z]{2}-[0-9]{3}$',ccode) is None:
-			messages.error(request,'Invalid Course Code entered')
-			return redirect('admin_home')
-		semester = request.POST.get('semester')
-		branch = request.POST.get('branch')
-		credits = request.POST.get('credit')
-		midsem = request.POST['midsem']
-		endsem = request.POST['endsem']
-		newcourse = Course.objects.create(course_name = cname,course_code=ccode,course_semester=semester,course_credits=credits,course_branch=branch)
-		newcourse.save()
-		mfdead = Deadlines.objects.create(course=newcourse,name='FD',desc='Mid-Semester Exam Feedback',date=midsem,code = ccode)
-		mfdead.save()
-		mfeed = Feedback.objects.create(course=newcourse,deadline=mfdead,name="Midsem Feedback")
-		question1 = Question.objects.create(question="Do you have any comments or suggestions you would like to share about the instructor or the course?",question_type="TF")
-		question1.save()
-		mfeed.question_set.add(question1)
-		question2 = Question.objects.create(question="Overall, the course was well organized.",question_type="RB")
-		question2.save()
-		mfeed.question_set.add(question2)
-		question3 = Question.objects.create(question="The instructor was well prepared for each class.",question_type="RB")
-		question3.save()
-		mfeed.question_set.add(question3)
-		question4 = Question.objects.create(question="Feedback on curriculum/course content",question_type="TF")
-		question4.save()
-		mfeed.question_set.add(question4)
-		mfeed.save()
+		if request.method == 'POST':
+			cname = request.POST['cname']
+			ccode = request.POST['ccode']
+			if re.match('^[A-Z]{2}-[0-9]{3}$',ccode) is None:
+				messages.error(request,'Invalid Course Code entered')
+				return redirect('admin_home')
+			semester = request.POST.get('semester')
+			branch = request.POST.get('branch')
+			credits = request.POST.get('credit')
+			midsem = request.POST['midsem']
+			endsem = request.POST['endsem']
+			newcourse = Course.objects.create(course_name = cname,course_code=ccode,course_semester=semester,course_credits=credits,course_branch=branch)
+			newcourse.save()
+			mfdead = Deadlines.objects.create(course=newcourse,name='FD',desc='Mid-Semester Exam Feedback',date=midsem,code = ccode)
+			mfdead.save()
+			mfeed = Feedback.objects.create(course=newcourse,deadline=mfdead,name="Midsem Feedback")
+			question1 = Question.objects.create(question="Do you have any comments or suggestions you would like to share about the instructor or the course?",question_type="TF")
+			question1.save()
+			mfeed.question_set.add(question1)
+			question2 = Question.objects.create(question="Overall, the course was well organized.",question_type="RB")
+			question2.save()
+			mfeed.question_set.add(question2)
+			question3 = Question.objects.create(question="The instructor was well prepared for each class.",question_type="RB")
+			question3.save()
+			mfeed.question_set.add(question3)
+			question4 = Question.objects.create(question="Feedback on curriculum/course content",question_type="TF")
+			question4.save()
+			mfeed.question_set.add(question4)
+			mfeed.save()
 
-		efdead = Deadlines.objects.create(course=newcourse,name='FD',desc='End-Semester Exams Feedback',date=endsem,code = ccode)
-		efdead.save()
-		efeed = Feedback.objects.create(course=newcourse,deadline=efdead,name="Endsem Feedback")
-		question1 = Question.objects.create(question="Do you have any comments or suggestions you would like to share about the instructor or the course?",question_type="TF")
-		question1.save()
-		efeed.question_set.add(question1)
-		question2 = Question.objects.create(question="Overall, the course was well organized.",question_type="RB")
-		question2.save()
-		efeed.question_set.add(question2)
-		question3 = Question.objects.create(question="The instructor was well prepared for each class.",question_type="RB")
-		question3.save()
-		efeed.question_set.add(question3)
-		question4 = Question.objects.create(question="Feedback on curriculum/course content",question_type="TF")
-		question4.save()
-		efeed.question_set.add(question4)
-		efeed.save()
-		mdead = Deadlines.objects.create(course=newcourse,name='EX',desc='Mid-Semester Exams',date=midsem,code = ccode)
-		mdead.save()
-		edead = Deadlines.objects.create(course=newcourse,name='EX',desc='End-Semester Exams',date=endsem,code = ccode)
-		edead.save()
-		messages.add_message(request, messages.SUCCESS, 'Congratulations.New Course has been added!')
-		messages.add_message(request,messages.INFO,'You can add new students to the course by clicking Add/Remove Students in Running Courses Menu')
-		return redirect('admin_home')
+			efdead = Deadlines.objects.create(course=newcourse,name='FD',desc='End-Semester Exams Feedback',date=endsem,code = ccode)
+			efdead.save()
+			efeed = Feedback.objects.create(course=newcourse,deadline=efdead,name="Endsem Feedback")
+			question1 = Question.objects.create(question="Do you have any comments or suggestions you would like to share about the instructor or the course?",question_type="TF")
+			question1.save()
+			efeed.question_set.add(question1)
+			question2 = Question.objects.create(question="Overall, the course was well organized.",question_type="RB")
+			question2.save()
+			efeed.question_set.add(question2)
+			question3 = Question.objects.create(question="The instructor was well prepared for each class.",question_type="RB")
+			question3.save()
+			efeed.question_set.add(question3)
+			question4 = Question.objects.create(question="Feedback on curriculum/course content",question_type="TF")
+			question4.save()
+			efeed.question_set.add(question4)
+			efeed.save()
+			mdead = Deadlines.objects.create(course=newcourse,name='EX',desc='Mid-Semester Exams',date=midsem,code = ccode)
+			mdead.save()
+			edead = Deadlines.objects.create(course=newcourse,name='EX',desc='End-Semester Exams',date=endsem,code = ccode)
+			edead.save()
+			messages.add_message(request, messages.SUCCESS, 'Congratulations.New Course has been added!')
+			messages.add_message(request,messages.INFO,'You can add new students to the course by clicking Add/Remove Students in Running Courses Menu')
+			return redirect('admin_home')
+		else:
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Don't try to be smart!! We ensure quite enough security!! :)")
 
 def update_admin(request):
 	if request.user.is_authenticated and request.user.username[0] == "a":
-		firstname = request.POST['firstname']
-		lastname = request.POST['lastname']
-		email = request.POST['email']
-		password = request.POST['password']
-		user = authenticate(username=request.user.username, password=password)
-		if user is not None:
-			user.first_name = firstname
-			user.last_name = lastname
-			user.email = email
-			user.username = 'a:'+email
-			user.save()
-			messages.success(request, 'Profile Successfully Updated')
-			return redirect('admin_home')
+		if request.method == 'POST':
+			firstname = request.POST['firstname']
+			lastname = request.POST['lastname']
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(username=request.user.username, password=password)
+			if user is not None:
+				user.first_name = firstname
+				user.last_name = lastname
+				user.email = email
+				user.username = 'a:'+email
+				user.save()
+				messages.success(request, 'Profile Successfully Updated')
+				return redirect('admin_home')
+			else:
+				messages.error(request, 'Wrong Password')
+				return redirect('admin_home')
 		else:
-			messages.error(request, 'Wrong Password')
-			return redirect('admin_home')
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Don't try to be smart!! We ensure quite enough security!! :)")
 
 def update_ins(request):
 	if request.user.is_authenticated and request.user.username[0] == "i":
-		firstname = request.POST['firstname']
-		lastname = request.POST['lastname']
-		email = request.POST['email']
-		branch = request.POST.get('branch')
-		username = 'i:'+email
-		try:
-			user = User.objects.get(username=username)
-			if not (user == request.user):
-				messages.error(request, 'User with this email already exits!')
+		if request.method == 'POST':
+			firstname = request.POST['firstname']
+			lastname = request.POST['lastname']
+			email = request.POST['email']
+			branch = request.POST.get('branch')
+			username = 'i:'+email
+			try:
+				user = User.objects.get(username=username)
+				if not (user == request.user):
+					messages.error(request, 'User with this email already exits!')
+					return redirect('ins_home')
+				user = request.user
+				user.first_name = firstname
+				user.last_name = lastname
+				user.save()
+				ins = Instructor.objects.get(user=user)
+				ins.instructor_branch = branch
+				ins.save()
+				messages.success(request, 'Profile Successfully Updated')
 				return redirect('ins_home')
-			user = request.user
-			user.first_name = firstname
-			user.last_name = lastname
-			user.save()
-			ins = Instructor.objects.get(user=user)
-			ins.instructor_branch = branch
-			ins.save()
-			messages.success(request, 'Profile Successfully Updated')
-			return redirect('ins_home')
-		except Exception as e:
-			user = request.user
-			user.first_name = firstname
-			user.last_name = lastname
-			user.email = email
-			user.username = username
-			user.save()
-			ins = Instructor.objects.get(user=user)
-			ins.instructor_branch = branch
-			ins.save()
-			messages.success(request, 'Profile Successfully Updated')
-			return redirect('ins_home')
+			except Exception as e:
+				user = request.user
+				user.first_name = firstname
+				user.last_name = lastname
+				user.email = email
+				user.username = username
+				user.save()
+				ins = Instructor.objects.get(user=user)
+				ins.instructor_branch = branch
+				ins.save()
+				messages.success(request, 'Profile Successfully Updated')
+				return redirect('ins_home')
+		else:
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Don't try to be smart!! We ensure quite enough security!! :)")
 
@@ -335,33 +344,39 @@ def instprofile(request):
 
 def make_feedback(request):
 	if request.user.is_authenticated and request.user.username[0] == "i":
-		fcourseid = request.POST['fcourse']
-		fname = request.POST['fname']
-		fdeadline = request.POST['deadline']
-		qcount = int(request.POST['qcount'])
-		course_concerned = Course.objects.get(course_code=fcourseid)
-		newdeadline = Deadlines.objects.create(course=course_concerned,name='FD',desc=fname,date=fdeadline,code=fcourseid)
-		newdeadline.save()
-		newfeedback = Feedback.objects.create(course=course_concerned,deadline=newdeadline,name=fname)
-		for i in range(qcount):
-			newquestion(question=request.POST['q'+str(i+1)],question_type=request.POST['t'+str(i+1)])
-			newquestion.save()
-			newfeedback.question_set.add(newquestion)
-		newfeedback.save()
-		return redirect(login)
+		if request.method == 'POST':
+			fcourseid = request.POST['fcourse']
+			fname = request.POST['fname']
+			fdeadline = request.POST['deadline']
+			qcount = int(request.POST['qcount'])
+			course_concerned = Course.objects.get(course_code=fcourseid)
+			newdeadline = Deadlines.objects.create(course=course_concerned,name='FD',desc=fname,date=fdeadline,code=fcourseid)
+			newdeadline.save()
+			newfeedback = Feedback.objects.create(course=course_concerned,deadline=newdeadline,name=fname)
+			for i in range(qcount):
+				newquestion(question=request.POST['q'+str(i+1)],question_type=request.POST['t'+str(i+1)])
+				newquestion.save()
+				newfeedback.question_set.add(newquestion)
+			newfeedback.save()
+			return redirect(login)
+		else:
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Don't try to be smart!! We ensure quite enough security!! :)")
 
 def make_deadline(request):
 	if request.user.is_authenticated and request.user.username[0] == "i":
-		dcourseid = request.POST['dcourse']
-		course_concerned = Course.objects.get(course_code=dcourseid)
-		typeofdeadline = request.POST['type']
-		details = request.POST['deadlinedetails']
-		deadlinedate = request.POST['deadlinedate']
-		newdeadline = Deadlines.objects.create(course=course_concerned,name=typeofdeadline,desc=details,date=deadlinedate,code=dcourseid)
-		newdeadline.save()
-		return redirect(login)
+		if request.method == 'POST':
+			dcourseid = request.POST['dcourse']
+			course_concerned = Course.objects.get(course_code=dcourseid)
+			typeofdeadline = request.POST['type']
+			details = request.POST['deadlinedetails']
+			deadlinedate = request.POST['deadlinedate']
+			newdeadline = Deadlines.objects.create(course=course_concerned,name=typeofdeadline,desc=details,date=deadlinedate,code=dcourseid)
+			newdeadline.save()
+			return redirect(login)
+		else:
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Over smart, huh??")
 
@@ -389,32 +404,38 @@ def add_stud_to_course(request,code):
 
 def modify(request):
 	if request.user.is_authenticated and request.user.username[0] == "i":
-		idstd = request.POST.get('id', None)
-		code = request.POST.get('code',None)
-		usr = User.objects.get(id = idstd)
-		if {'course_code' : code} in usr.student.course_set.values('course_code'):
-			usr.student.course_set.remove(Course.objects.get(course_code = code))
+		if request.method == 'POST':
+			idstd = request.POST.get('id', None)
+			code = request.POST.get('code',None)
+			usr = User.objects.get(id = idstd)
+			if {'course_code' : code} in usr.student.course_set.values('course_code'):
+				usr.student.course_set.remove(Course.objects.get(course_code = code))
+			else:
+				usr.student.course_set.add(Course.objects.get(course_code=code))
+			return error(request,"Modifications were successful! However you were not redirected! :p")
 		else:
-			usr.student.course_set.add(Course.objects.get(course_code=code))
-		return error(request,"Modifications were successful! However you were not redirected! :p")
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Don't try to be smart!! We ensure quite enough security!! :)")
 
 def updatedatabase(request):
-	if request.user.is_authenticated and request.user.username[0] == "i":
-		csvf = request.FILES["studentscsv"]
-		for a in csvf:
-			row = a.decode("utf-8").split(',')
-			if {'username' : "s:" + row[0]} in User.objects.all().values('username'):
-				continue
-			else:
-				usr = User.objects.create_user("s:"+row[0],row[0],row[1])
-				usr.first_name = row[3]
-				usr.last_name = row[4]
-				usr.save()
-				newstudent = Student.objects.create(user = usr,roll_number = row[2])
-				newstudent.save()
-		return redirect('login')
+	if request.user.is_authenticated and request.user.username[0] == "a":
+		if request.method == 'POST':
+			csvf = request.FILES["studentscsv"]
+			for a in csvf:
+				row = a.decode("utf-8").split(',')
+				if {'username' : "s:" + row[0]} in User.objects.all().values('username'):
+					continue
+				else:
+					usr = User.objects.create_user("s:"+row[0],row[0],row[1])
+					usr.first_name = row[3]
+					usr.last_name = row[4]
+					usr.save()
+					newstudent = Student.objects.create(user = usr,roll_number = row[2])
+					newstudent.save()
+			return redirect('login')
+		else:
+			return error(request,"Don't try random urls!")
 	else:
 		return error(request,"Don't try to be smart!! We ensure quite enough security!! :)")
 
