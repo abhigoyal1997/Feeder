41621a9e245554d7cc5da38cfc5a829968531662
SHESHANSH AGRAWAL <sheshansh@cse.iitb.ac.in>
2016-11-05 21:51:05 +0530

    premerge

--- lab10/FeederApp/static/js/signup.js
+++
@@ -1,8 +0,0 @@
-function changerole() {
-	if ($("#student").is(':checked')) {
-		$(".student").removeClass("hidden");
-	}
-	else if ($("#instructor").is(':checked')) {
-		$(".student").addClass("hidden");
-	}
-}
